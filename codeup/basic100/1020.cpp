b4c05f65b109d567ed01885c3373177f82b92d52
LEE MYEONG JIN <71704247+NoirNorie@users.noreply.github.com>
2022-04-28 16:18:40 +0900

    Create 1020.cpp

---
+++ codeup/basic100/1020.cpp
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+
+int main()
+{
+	//ios_base::sync_with_stdio(false);
+	//cin.tie(0); cout.tie(0);
+
+	int a, b;
+	scanf("%d - %d", &a, &b);
+	printf("%06d%06d", a, b);
+}
